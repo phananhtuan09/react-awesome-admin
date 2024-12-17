01c557441ab2bd2f175408e40eb47819e8fdf726
phananhtuan <tuanp6905@gmail.com>
2024-12-17 23:28:14 +0700

    add dynamic path generation and update routing constants; define new routes for admin and user management

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -9,4 +9,97 @@ const PATH = {
   PROFILE: 'profile',
 };
 
+type Route = {
+  path: string;
+  name: string;
+  subRoutes?: ReadonlyArray<Route>;
+};
+
+type GenerateKeys<
+  T extends ReadonlyArray<Route>,
+  Prefix extends string = '',
+> = {
+  [K in keyof T]: T[K] extends Route
+    ? T[K]['subRoutes'] extends ReadonlyArray<Route>
+      ? // Include current key and recursively generate keys for subRoutes
+        | `${Prefix}${T[K]['name']}`
+          | GenerateKeys<T[K]['subRoutes'], `${Prefix}${T[K]['name']}_`>
+      : // If no subRoutes, only include the current key
+        `${Prefix}${T[K]['name']}`
+    : never;
+}[number];
+
+function generatePath<T extends ReadonlyArray<Route>>(
+  paths: T,
+  prefixPath = '',
+  prefixName = '',
+): { [K in GenerateKeys<T>]: string } {
+  const result = {} as { [K in GenerateKeys<T>]: string };
+
+  if (!Array.isArray(paths)) {
+    return result;
+  }
+
+  paths.forEach((path) => {
+    const pathName = prefixName ? `${prefixName}_${path.name}` : path.name;
+    const newPath = `${prefixPath}/${path.path}`;
+    result[pathName as GenerateKeys<T>] = newPath;
+
+    if (path.subRoutes) {
+      Object.assign(result, generatePath(path.subRoutes, newPath, pathName));
+    }
+  });
+
+  return result;
+}
+
+// Your route definition
+const definePath = [
+  {
+    path: '',
+    name: 'ROOT',
+  },
+  {
+    path: 'login',
+    name: 'LOGIN',
+  },
+  {
+    path: 'register',
+    name: 'REGISTER',
+  },
+  {
+    path: 'admin',
+    name: 'ADMIN',
+    subRoutes: [
+      {
+        path: 'home',
+        name: 'HOME',
+      },
+      {
+        path: 'profile',
+        name: 'PROFILE',
+      },
+      {
+        path: 'users',
+        name: 'USERS',
+        subRoutes: [
+          {
+            path: 'add',
+            name: 'ADD_USER',
+          },
+          {
+            path: 'edit/:id',
+            name: 'EDIT_USER',
+          },
+        ],
+      },
+    ],
+  },
+] as const;
+
+// Generate the PATH object with dynamic typing
+const PATH2 = generatePath(definePath);
+
+export { PATH2 };
+
 export default PATH;
--- src/routes/mainRoutes.ts
+++ src/routes/mainRoutes.ts
@@ -1,11 +1,13 @@
 import { RouteObject } from 'react-router';
 import { lazy, createElement } from 'react';
 import LazyWrapper from '@/components/ui/lazyWrapper';
-import PATH from '@/constants/path';
+import PATH, { PATH2 } from '@/constants/path';
 
 const HomePage = LazyWrapper(lazy(() => import('@/pages/home')));
 const ProfilePage = LazyWrapper(lazy(() => import('@/pages/profile')));
 const MainLayout = LazyWrapper(lazy(() => import('@/layout/mainLayout')));
+
+console.log('PATH2', PATH2);
 
 const mainRoutes: RouteObject = {
   path: PATH.ROOT,
